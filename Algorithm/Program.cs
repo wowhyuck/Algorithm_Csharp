432bd38946bb0b19c35b45c8c71c67f1a03f03dd
Joohyuck <joohyuck.seo@gmail.com>
2021-02-24 06:57:00 +0900

    환경 설정

---
+++ Algorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+
+            while(true)
+            {
+                #region 프레임 관리
+                int currentTick = System.Environment.TickCount;
+
+                // 만약 경과한 시간이 1/30초보다 작다면
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+                // 로직
+                // 렌더링
+
+                Console.SetCursorPosition(0, 0);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
