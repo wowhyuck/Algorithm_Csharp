9d4eedbb23b995adfa56eb1bb360e5337e2fd770
Joohyuck <joohyuck.seo@gmail.com>
2021-02-24 15:36:54 +0900

    연결 리스트 구현 연습

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -47,24 +47,81 @@ namespace Algorithm
         }
     }
 
+    class MyLinkedListNode<T>
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
+
+    
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null;        // 첫번째
+        public MyLinkedListNode<T> Tail = null;        // 마지막
+        public int Count = 0;
+
+        // O(1)
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+
+            // 만약에 아직 방에 없었다면, 새로 추가한 첫번째 방이 곧 Head이다.
+            if (Head == null)
+                Head = newRoom;
+
+            // 기존의 [마지막 방]과 [새로 추가된 방]을 연결해준다.
+            if (Tail != null)
+            {
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
+            }
+
+            // [새로 추가된 방]을 [마지막 방]으로 인정한다.
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+
+        // O(1)
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            // [기존의 첫번째 방의 다음 방]을 [첫번째의 방]로 인정한다.
+            if (Head == room)
+                Head = Head.Next;
+
+            // [기존의 마지막 방의 이전 방]을 [마지막 방]으로 인정한다.
+            if (Tail == room)
+                Tail = Tail.Prev;
+
+            if (room.Prev != null)
+                room.Prev.Next = room.Next;
+
+            if (room.Next != null)
+                room.Next.Prev = room.Prev;
+
+            Count--;
+        }
+    }
+
     class Board
     {
         public int[] _data = new int[25];                       // 배열
         //public List<int> _data2 = new List<int>();              // 동적 배열, C++에서 vector
         public MyList<int> _data2 = new MyList<int>();          
-        public LinkedList<int> _data3 = new LinkedList<int>();  // 연결 리스트
+        //public LinkedList<int> _data3 = new LinkedList<int>();  // 연결 리스트
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>();  // 연결 리스트
 
         public void Initialize()
         {
-            _data2.Add(101);
-            _data2.Add(102);
-            _data2.Add(103);
-            _data2.Add(104);
-            _data2.Add(105);
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
 
-            int temp = _data2[2];
-
-            _data2.RemoveAt(2);
+            _data3.Remove(node);
         }
     }
 }
