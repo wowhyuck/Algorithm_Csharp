1aacf1e19a3b22a9f498d762e7006d515e3c265f
Joohyuck <joohyuck.seo@gmail.com>
2021-02-24 12:03:10 +0900

    배열, 동적 배열, 연결 리스트 비교

---
+++ Algorithm/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Board
+    {
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+
+        public void Initialize()
+        {
+        }
+    }
+}
