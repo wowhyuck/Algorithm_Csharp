353f422c4aab3dd74df9c136f0c7411b5db5998b
Joohyuck <joohyuck.seo@gmail.com>
2021-02-26 18:04:02 +0900

    미로 생성 알고리즘 (Binary Tree & Sidewinder)

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -118,17 +118,120 @@ namespace Algorithm
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
+            // Mazes for Programmers
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
+        }
+
+        void GenerateByBinaryTree()
+        {
+            // 일단 길을 다 막아버리는 작업
             for (int y = 0; y < 25; y++)
             {
                 for (int x = 0; x < 25; x++)
                 {
-                    if (x == 0 || x == _size - 1 || y == 0 || y == _size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            // Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < 25; y++)
+            {
+                for (int x = 0; x < 25; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        void GenerateBySideWinder()
+        {
+            // 일단 길을 다 막아버리는 작업
+            for (int y = 0; y < 25; y++)
+            {
+                for (int x = 0; x < 25; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            // Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < 25; y++)
+            {
+                int count = 1;
+
+                for (int x = 0; x < 25; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
                 }
             }
         }
