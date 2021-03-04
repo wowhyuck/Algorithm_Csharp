a45e239512d74d0defc73eb14be718212246ae4f
Joohyuck <joohyuck.seo@gmail.com>
2021-03-05 02:44:34 +0900

    스택과 큐

---
+++ Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    // 스택: LIFO(후입선출 Last in First out)
+    // 큐: FIFO(선입선출 First in First out)
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(101);
+            stack.Push(102);
+            stack.Push(103);
+            stack.Push(104);
+            stack.Push(105);
+
+            int data = stack.Pop();
+            int data2 = stack.Peek();
+
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(101);
+            queue.Enqueue(102);
+            queue.Enqueue(103);
+            queue.Enqueue(104);
+            queue.Enqueue(105);
+
+            int data3 = queue.Dequeue();
+            int data4 = queue.Peek();
+        }
+    }
+}
