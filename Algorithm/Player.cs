5e6485cb083adcc44ef62a0c2593511c5fed3aae
Joohyuck <joohyuck.seo@gmail.com>
2021-03-06 03:36:22 +0900

    BFS를 이용한 길찾기 구현

--- Algorithm/Player.cs
+++ Algorithm/Player.cs
@@ -36,6 +36,63 @@ namespace Algorithm
 
             _board = board;
 
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0};
+            int[] deltaX = new int[] { 0, -1, 0, 1};
+
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+
+            while (parent[y, x].Y != y || parent[y, x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+        }
+
+        void RightHand()
+        {
             // 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
             int[] frontY = new int[] { -1, 0, 1, 0 };
             int[] frontX = new int[] { 0, -1, 0, 1 };
@@ -45,7 +102,7 @@ namespace Algorithm
             _points.Add(new Pos(PosY, PosX));
 
             // 목적지에 도착하기 전에는 계속 실행
-            while(PosY != board.DestY || PosX !=board.DestX)
+            while (PosY != _board.DestY || PosX != _board.DestX)
             {
                 // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
                 if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
@@ -60,7 +117,7 @@ namespace Algorithm
                 }
 
                 // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
-                else if(_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
                 {
                     // 앞으로 한 보 전진
                     PosY = PosY + frontY[_dir];
@@ -68,12 +125,13 @@ namespace Algorithm
                     _points.Add(new Pos(PosY, PosX));
                 }
 
-                else 
+                else
                 {
                     // 왼쪽 방향으로 90도 회전
                     _dir = (_dir + 1 + 4) % 4;
                 }
             }
+
         }
 
         const int MOVE_TICK = 10;
