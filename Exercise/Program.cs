027986fd17cabda2d23cce05f8cb339c1054f5ed
Joohyuck <joohyuck.seo@gmail.com>
2021-03-12 08:20:50 +0900

    우선순위 큐

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Exercise
 {
@@ -187,6 +188,99 @@ namespace Exercise
     }
     #endregion
 
+    #region Priority Queue
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+        List<T> _heap = new List<T>();
+
+        // O(logN)
+        public void Push(T data)
+        {
+            // 힙의 맨 끝의 새로운 데이터를 삽입한다.
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+
+            // 도장깨기 시작
+            while (now > 0)
+            {
+                // 도장깨기 시도
+                int next = (now - 1) / 2;
+                if (_heap[now].CompareTo(_heap[next]) < 0)
+                    break;      // 실패
+
+                // 두 값을 교체한다
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // 검사 위치를 이동한다
+                now = next;
+            }
+        }
+
+        // O(logN)
+        public T Pop()
+        {
+            // 반환할 데이터를 따로 저장
+            T ret = _heap[0];
+
+            // 마지막 데이터를 루트로 이동
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            // 역으로 내려가는 도장깨기 시작
+            int now = 0;
+            while (true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                // 왼쪽값이 현재값보다 더 크면, 왼쪽으로 이동
+                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+                    next = left;
+                // 오른쪽값이 현재값(왼쪽 이동 포함)보다 더 크면, 오른쪽으로 이동
+                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+                    next = right;
+
+                // 왼쪽/오른쪽 모두 현재값보다 작으면 종료
+                if (next == now)
+                    break;
+
+                // 두 값을 교체한다.
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // 검사 위치를 이동한다
+                now = next;
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
+    }
+
+    class Knight : IComparable<Knight>
+    {
+        public int Id { get; set; }
+
+        public int CompareTo(Knight other)
+        {
+            if (Id == other.Id)
+                return 0;
+            return Id > other.Id ? 1 : -1;
+        }
+    }
+    #endregion
+
     class Program
     {
         static TreeNode<string> MakeTree()
@@ -248,6 +342,8 @@ namespace Exercise
 
         static void Main(string[] args)
         {
+            /* 그래프 ----------------------------------------------*/
+
             // DFS (Depth First Search : 깊이 우선 탐색)
             // BFS (Breadth First Search : 너비 우선 탐색)
 
@@ -256,11 +352,39 @@ namespace Exercise
             //graph.DFS2(3);
             //graph.BFS(0);
 
-            /*----------------------------------------------*/
+            /* 트리 ----------------------------------------------*/
 
-            TreeNode<string> root = MakeTree();
-            //PrintTree(root);
-            Console.WriteLine(GetHeight(root));
+            //TreeNode<string> root = MakeTree();
+            ////PrintTree(root);
+            //Console.WriteLine(GetHeight(root));
+
+            /* 우선순위 큐 ----------------------------------------------*/
+
+            PriorityQueue<int> q = new PriorityQueue<int>();
+            q.Push(20);
+            q.Push(10);
+            q.Push(30);
+            q.Push(90);
+            q.Push(40);
+
+            while (q.Count() > 0)
+            {
+                Console.WriteLine(q.Pop());
+            }
+
+            Console.WriteLine();
+
+            PriorityQueue<Knight> knight = new PriorityQueue<Knight>();
+            knight.Push(new Knight() { Id = 20 });
+            knight.Push(new Knight() { Id = 30 });
+            knight.Push(new Knight() { Id = 40 });
+            knight.Push(new Knight() { Id = 10 });
+            knight.Push(new Knight() { Id = 05 });
+
+            while (knight.Count() > 0)
+            {
+                Console.WriteLine(knight.Pop().Id);
+            }
         }
     }
 }
